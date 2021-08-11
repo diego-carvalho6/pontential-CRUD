7a18222858a5dd7ed79677f642ed9aeb728da221
diego <kiritokirigayakazuto1@gmail.com>
2021-08-10 22:33:43 -0300

    feat: created model developer

--- composeexample/asgi.py
+++ composeexample/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for potential_crud project.
+ASGI config for composeexample project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'potential_crud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'composeexample.settings')
 
 application = get_asgi_application()
--- developers/models.py
+++ developers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+class Developer(models.Model):
+    nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1)
+    idade = models.IntegerField()
+    hobby = models.CharField(max_length=100)
+    datanascimento = models.DateField()
